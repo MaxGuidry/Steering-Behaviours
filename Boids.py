3604aa5c937ccf7e4d06b18e636663ed9f584439
MaxGuidry <mguidry7@outlook.com>
2017-03-22 20:56:03 -0500

    implemented the flee behaviour

--- Boids.py
+++ Boids.py
@@ -5,13 +5,14 @@ import vectoroperations as vec
 class Boid(object):
     """Class Boid."""
 
-    def __init__(self):
+    def __init__(self, positionbound):
         self.position = (0, 0)
         self.velocity = (0, 0)
         self.acceleration = (0, 0)
         self.maxvelo = 20
         self.target = None
         self.forceapplied = (0, 0)
+        self.bounds = positionbound
 
     def seek(self, scalingfactor):
         """Seeking Behaviour."""
@@ -23,7 +24,12 @@ class Boid(object):
         self._updateposition()
 
     def flee(self, scalingfactor):
-        pass
+        displacement = vec.get_dist(self.position, self.target.position)
+        direction = vec.get_normalized(displacement)
+        self._addforce((direction[0] * ((1 / vec.get_magnitude(displacement)) * self.bounds[0] * self.bounds[0]) * -200, direction[1] * ((1 / vec.get_magnitude(displacement)) * self.bounds[1] * self.bounds[1]) * -200))
+        self._updateacceleration(scalingfactor)
+        self._updatevelocity(scalingfactor)
+        self._updateposition()
 
     def wander(self, scalingfactor):
         pass
@@ -38,11 +44,11 @@ class Boid(object):
         pass
 
     def _addforce(self, forceapplied):
-        self.forceapplied=(forceapplied[0],
+        self.forceapplied = (forceapplied[0],
                              forceapplied[1])
 
     def _updateacceleration(self, deltatime):
-        self.acceleration = (self.forceapplied[0]* deltatime,
+        self.acceleration = (self.forceapplied[0] * deltatime,
                              self.forceapplied[1] * deltatime)
 
     def _updatevelocity(self, deltatime):
@@ -54,3 +60,11 @@ class Boid(object):
     def _updateposition(self):
         self.position = (self.position[0] + self.velocity[0],
                          self.position[1] + self.velocity[1])
+        if self.position[0] < 10:
+            self.position = (10, self.position[1])
+        if self.position[1] < 10:
+            self.position = (self.position[0], 10)
+        if self.position[0] > self.bounds[0] - 10:
+            self.position = (self.bounds[0] - 10, self.position[1])
+        if self.position[1] > self.bounds[1] - 10:
+            self.position = (self.position[0], self.bounds[1] - 10)
--- main.py
+++ main.py
@@ -12,10 +12,10 @@ if __name__ == "__main__":
     pygame.display.set_mode((1080, 720))
     SCREEN = pygame.display.get_surface()
     BOIDS = []
-    TARGETBOID = boids.Boid()
+    TARGETBOID = boids.Boid((SCREEN.get_width(), SCREEN.get_height()))
     TARGETBOID.position = (SCREEN.get_width() / 2, SCREEN.get_height() / 2)
     for itera in range(10):
-        BOIDS.append(boids.Boid())
+        BOIDS.append(boids.Boid((SCREEN.get_width(), SCREEN.get_height())))
         BOIDS[itera].position = (random.randrange(0, SCREEN.get_width() + 1),
                                  random.randrange(0, SCREEN.get_height() + 1))
         BOIDS[itera].target = TARGETBOID
